25e2f953393f4cbf3617c1ab48ea451dd268e9b3
joshstclair <joshstclair@ucla.edu>
2015-11-11 01:27:00 -0800

    converted python 2 to python 3 and modified CKY algorithm to display top two parsings

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from stat_parser import Parser, display_tree
 
 parser = Parser()
 
-# http://www.thrivenotes.com/the-last-question/
-tree = parser.parse("How can the net amount of entropy of the universe be massively decreased?")
+[tree1, tree2] = parser.parse("John saw Mary with the telescope")
 
-display_tree(tree)
+display_tree(tree1)
+display_tree(tree2)
